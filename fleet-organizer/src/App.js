ebfedbb8105c706ac76df26aa4521d133b7b680c
Wojciech Rydel <rydel.wojciech@gmail.com>
2018-11-15 14:43:06 +0100

    Change App component to function

--- fleet-organizer/src/App.js
+++ fleet-organizer/src/App.js
@@ -1,30 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="fleet-header">
-          <h2>Fleet Organizer</h2>
-        </header>
-        <div className="fleet-register">
-          <h4>Registered Cars</h4>
-          <div className="controls">
-            <button>Sort by name</button>
-          </div>
-          <ul className="car-list">
-            <li>
-              <div className="car-info">
-              First car
-              <span className="car-registration">WAW 12375</span>
-              </div>
-            </li>
-          </ul>
+function App() {
+  return (
+    <div className="App">
+      <header className="fleet-header">
+        <h2>Fleet Organizer</h2>
+      </header>
+      <div className="fleet-register">
+        <h4>Registered Cars</h4>
+        <div className="controls">
+          <button>Sort by name</button>
         </div>
+        <ul className="car-list">
+          <li>
+            <div className="car-info">
+            First car
+            <span className="car-registration">WAW 12375</span>
+            </div>
+          </li>
+        </ul>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
