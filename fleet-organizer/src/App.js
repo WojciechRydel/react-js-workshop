b62516963711c755dfc0ed472d74eb7291bb9018
Wojciech Rydel <rydel.wojciech@gmail.com>
2018-11-29 13:19:05 +0100

    End 2nd lesson

--- fleet-organizer/src/App.js
+++ fleet-organizer/src/App.js
@@ -1,25 +1,60 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="fleet-header">
-        <h2>Fleet Organizer</h2>
-      </header>
-      <div className="fleet-register">
-        <h4>Registered Cars</h4>
-        <ul className="car-list">
-          <li>
-            <div className="car-info">
-            First car
-            <span className="car-registration">WAW 12375</span>
-            </div>
-          </li>
-        </ul>
+class CarInfo extends React.PureComponent {
+  render() {
+   return (
+      <li >
+       <div className="car-info"> {this.props.car.name}
+            <span className="car-registration">{this.props.car.number}</span>
+        </div>
+      </li>
+    )
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      cars : [
+        {name: 'First car', number: 'WAW 123545'},
+        {name: 'No Registered car'},
+        {name: 'Second car', number: 'WAW 123545'}
+      ]
+    }
+
+  }
+
+  addCar = () => {
+    this.setState({
+      cars: [
+        ...this.state.cars,
+        {name: 'Some New Car', number: new Date().toISOString()}
+      ]
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="fleet-header">
+          <h2>Fleet Organizer</h2>
+        </header>
+        <div className="fleet-register">
+          <h4>Registered Cars</h4>
+          <div className="controls">
+            <button onClick={this.addCar}>Add car</button>
+          </div>
+          <ul className="car-list">
+          { this.state.cars.filter(car => car.number ).map((car, index) => (
+            <CarInfo car={car} key={index}/>
+          )) }
+          </ul>
+        </div>
       </div>
-    </div>
-  );
+    )
+  }
 }
 
 export default App;
